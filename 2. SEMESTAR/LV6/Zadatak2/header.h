53cedf3901f8a199854f129c9acf25a462bfb293
Campi-i <78502958+Campi-i@users.noreply.github.com>
2022-04-11 10:18:38 +0200

    Create header.h

---
+++ 2. SEMESTAR/LV6/Zadatak2/header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+float** zauzimanje(int, int);
+void generiranje(int , int, float**);
+void ispis(int, int, float**);
+void ispis2(int , int ,float **, FILE *);
+
+#endif // HEADER_H
