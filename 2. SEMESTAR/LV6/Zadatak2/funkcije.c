072fb7ccb6ad7556342a5e8a7d5f271ad4f518b6
Campi-i <78502958+Campi-i@users.noreply.github.com>
2022-04-11 10:18:19 +0200

    Create funkcije.c

---
+++ 2. SEMESTAR/LV6/Zadatak2/funkcije.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "header.h"
+
+float** zauzimanje(int num1, int num2)
+{
+    float **matrica = NULL;
+    matrica = (float**)calloc(num1, sizeof(float*));
+
+    if(matrica == NULL){
+        return NULL;
+    }
+
+    for(int i = 0; i < num2; i++)
+    {
+        *(matrica + i) = (float*)calloc(num2, sizeof(float));
+
+        if(*(matrica+i) == NULL){
+            return NULL;
+        }
+    }
+
+    return matrica;
+}
+
+void generiranje(int num1, int num2, float **p){
+
+    for(int i = 0; i < num1; i++){
+
+        for(int j = 0; j < num2; j++){
+            *(*(p + i)+j) = -125.5 + (float)rand() / RAND_MAX * (65.8 + 125.5);
+        }
+    }
+}
+void ispis(int num1, int num2, float **p){
+
+    for(int i = 0; i < num1; i++){
+
+        for(int j = 0; j < num2; j++){
+           printf("%f ", *(*(p + i) + j));
+        }
+        printf("\n");
+    }
+}
+void ispis2(int num1, int num2,float **p, FILE *fp1){
+
+    for(int i = 0; i < num1 - 1; i++){
+
+        for(int j = 1; j < num2; j++){
+           fprintf(fp1, "%f  ", *(*(p + i) + j));
+        }
+        fprintf(fp1, "\n");
+    }
+}
+
